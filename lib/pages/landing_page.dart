8618ad3b5c137aec5e48b438b4d35057d4adb83a
Nihal Yadav <nihalyadav8524@gmail.com>
2024-06-20 11:56:07 +0530

    Changes

---
+++ lib/pages/landing_page.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:mnnit/pages/home_page.dart';
+import 'package:mnnit/pages/profile_page.dart';
+import 'package:mnnit/pages/sell_page.dart';
+
+class LandingPage extends StatefulWidget {
+  const LandingPage({super.key});
+
+  @override
+  State<LandingPage> createState() => _LandingPageState();
+}
+
+List<Widget> Tabs = [HomePage(), ProfilePage(), SellPage()];
+
+class _LandingPageState extends State<LandingPage> {
+  int currentTabIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Tabs[currentTabIndex],
+      bottomNavigationBar: NavigationBar(
+        onDestinationSelected: (int index) {
+          setState(() {
+            currentTabIndex = index;
+          });
+        },
+        indicatorColor: Colors.deepPurple.shade200,
+        selectedIndex: currentTabIndex,
+        destinations: const <Widget>[
+          NavigationDestination(
+            selectedIcon: Icon(Icons.home),
+            icon: Icon(Icons.home_outlined),
+            label: 'Home',
+          ),
+          NavigationDestination(
+            selectedIcon: Icon(Icons.person),
+            icon: Icon(Icons.person_outline),
+            label: 'Profile',
+          ),
+          NavigationDestination(
+            selectedIcon: Icon(Icons.outbox),
+            icon: Icon(Icons.outbox_outlined),
+            label: 'Sell',
+          ),
+          // NavigationDestination(
+          //   //selectedIcon: Icon(),
+          //   icon: Icon(Icons.settings),
+          //   label: 'Settings',
+          // ),
+        ],
+      ),
+    );
+  }
+}
