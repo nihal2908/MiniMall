91094269e9609bfb7e93436c78526061b2e7660d
Nihal Yadav <nihalyadav8524@gmail.com>
2024-06-26 01:12:59 +0530

    Some bug fixes in chatpage

--- lib/pages/caht_page.dart
+++ lib/pages/caht_page.dart
@@ -17,6 +17,7 @@ class _ChatPageState extends State<ChatPage> {
   final FirebaseFirestore firestore = FirebaseFirestore.instance;
   final Auth auth = Auth();
   final Firebase storage = Firebase();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -41,17 +42,24 @@ class _ChatPageState extends State<ChatPage> {
           }
           final userDoc = snapshot.data!;
 
+          // Check if the 'chats' field exists
+          if (!userDoc.data()!.containsKey('chats')) {
+            return const ListTile(
+              title: Text('You have no recent chats. Chats with product dealers will be displayed here.'),
+            );
+          }
+
           final List<dynamic> ids = userDoc['chats'];
           return SingleChildScrollView(
-              child: ChatList(ids: ids)
+            child: ChatList(ids: ids),
           );
-        }
-      )
+        },
+      ),
     );
   }
 
-  Widget ChatList({required List<dynamic> ids}){
-    if(ids.isEmpty) {
+  Widget ChatList({required List<dynamic> ids}) {
+    if (ids.isEmpty) {
       return const Center(
         child: Padding(
           padding: EdgeInsets.all(20.0),
@@ -63,17 +71,17 @@ class _ChatPageState extends State<ChatPage> {
       );
     }
     return FutureBuilder(
-        future: firestore.collection('users').where(FieldPath.documentId, whereIn: ids).get(),
-        builder: (context, snapshot){
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return CenterIndicator();
-          }
-          if (snapshot.hasError) {
-            return ListTile(
-              title: Text('Error: ${snapshot.error}'),
-            );
-          }
-          return Column(
+      future: firestore.collection('users').where(FieldPath.documentId, whereIn: ids).get(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return CenterIndicator();
+        }
+        if (snapshot.hasError) {
+          return ListTile(
+            title: Text('Error: ${snapshot.error}'),
+          );
+        }
+        return Column(
           children: snapshot.data!.docs.map((element) {
             String name = element.data()['name'];
             String id = element.id;
@@ -83,49 +91,51 @@ class _ChatPageState extends State<ChatPage> {
                 title: Text(name),
                 trailing: IconButton(
                   icon: const Icon(Icons.delete),
-                  onPressed: (){
+                  onPressed: () {
                     showDeleteDialog(context, id);
                   },
                 ),
                 onTap: () {
                   Navigator.push(
                     context,
-                    MaterialPageRoute(builder: (context) => ChatRoomPage(recieverId: id, name: name,)),
+                    MaterialPageRoute(
+                      builder: (context) => ChatRoomPage(recieverId: id, name: name),
+                    ),
                   );
                 },
               ),
             );
           }).toList(),
-          );
-        }
+        );
+      },
     );
   }
 
   void showDeleteDialog(BuildContext context, String id) {
     showDialog(
-        context: context,
-        builder: (context){
-          return AlertDialog(
-            title: Text('Delete Conversation?'),
-            content: Text('This will permanentlty delete all the conversation with this user!'),
-            actions: [
-              ElevatedButton(
-                  onPressed: (){
-                    Navigator.pop(context);
-                  },
-                  child: Text('Cancel', style: TextStyle(color: Colors.green),)
-              ),
-              ElevatedButton(
-                  onPressed: (){
-                    storage.deleteChat(id: id);
-                    setState(() {});
-                    Navigator.pop(context);
-                  },
-                  child: Text('Delete', style: TextStyle(color: Colors.red),)
-              ),
-            ],
-          );
-        }
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Delete Conversation?'),
+          content: Text('This will permanently delete all the conversation with this user!'),
+          actions: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text('Cancel', style: TextStyle(color: Colors.green)),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                storage.deleteChat(id: id);
+                setState(() {});
+                Navigator.pop(context);
+              },
+              child: Text('Delete', style: TextStyle(color: Colors.red)),
+            ),
+          ],
+        );
+      },
     );
   }
 }
